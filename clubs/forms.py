363dfe82629ab0a46d6106abd5ea319c1daf3c7e
shahinul22 <shahinul.alam007@gmail.com>
2025-04-28 23:54:55 +0600

    base

--- clubs/forms.py
+++ clubs/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from .models import CustomUser
 from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
 
 
 class OrganizerRegisterForm(UserCreationForm):
