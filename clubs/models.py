93993c40eaaab40c4fe85c38f16990d96393cf71
shahinul22 <shahinul.alam007@gmail.com>
2025-05-02 03:23:22 +0600

    null

--- clubs/models.py
+++ clubs/models.py
@@ -7,3 +7,15 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+    
+from django.db import models
+from django.conf import settings
+
+class OrganizerProfile(models.Model):
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    club_name = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    # add more fields as needed
+
+    def __str__(self):
+        return f"{self.user.username} - {self.club_name}"
--- jahangirnagar_club/settings.py
+++ jahangirnagar_club/settings.py
@@ -29,6 +29,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     # apps
     'clubs',
+    "widget_tweaks",
 ]
 
 AUTH_USER_MODEL = 'clubs.CustomUser'
@@ -120,3 +121,8 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+# media files
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
